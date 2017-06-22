a7a7817ee7e3a373984abe0f7a14fd64cba62c41
posenato <posenato@782453a1-1937-45d1-8845-2a6fcc2839b7>
2017-06-22 13:22:47 +0000

    tweaking

--- CSTNU/trunk/src/test/java/it/univr/di/labeledvalue/LabeledIntHierarchyMapTest.java
+++ CSTNU/trunk/src/test/java/it/univr/di/labeledvalue/LabeledIntHierarchyMapTest.java
@@ -115,7 +115,7 @@ public class LabeledIntHierarchyMapTest {
 				sb.append("(");
 				sb.append(e.getKey().toString());
 				sb.append(", ");
-				final int value = e.getValue();
+				final int value = e.getIntValue();
 				if ((value == Constants.INT_NEG_INFINITE) || (value == Constants.INT_POS_INFINITE)) {
 					if (value < 0) {
 						sb.append('-');
