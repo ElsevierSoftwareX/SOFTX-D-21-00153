b6a73bdc8788e6c9ff96aee61b99b894ff0f600c
posenato <posenato@782453a1-1937-45d1-8845-2a6fcc2839b7>
2018-01-10 10:16:02 +0000

    Remove bellman, reintegrated with trunk@243

--- CSTNU/branches/bellman/src/main/java/it/univr/di/cstnu/algorithms/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains all checking algorithms for CSTN & c. models.
- * 
- * @author posenato
- */
-package it.univr.di.cstnu.algorithms;
--- CSTNU/branches/bellman/src/main/java/it/univr/di/cstnu/graph/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains all classes for representing vertices, edges and (distance) graphs related to CSTN(U) model.
- * 
- * @author posenato
- */
-package it.univr.di.cstnu.graph;
