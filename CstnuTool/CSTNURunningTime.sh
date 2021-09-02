cbf63433a3f08bfc5bbd624ea51357360895370a
posenato <roberto.posenato@univr.it>
2021-09-02 14:36:02 +0000

    Fixed some minor bugs about copyright and possible outputs. Improved pom.xml

--- CstnuTool/CSTNURunningTime.sh
+++ CstnuTool/CSTNURunningTime.sh
@@ -16,12 +16,14 @@ CSTN="$DIR/$( cd $DIR  && echo `ls CSTNU-*.jar`)"
 #############################
 #Check Java version
 # Changed code to remove the 'head -1' as per the suggestion in comment.
-JAVA_VERSION=`java -version 2>&1 | head -n 1 | cut -d\" -f 2`
-JAVA_WANTED='1.8'
-if [[ $JAVA_VERSION != $JAVA_WANTED* ]]
+JAVA_MIN='1.8'
+JAVA_MIN_1='11'
+JAVA_VERSION_COMPLETE=`java -version 2>&1 | head -n 1 | cut -d\" -f 2`
+JAVA_VERSION_MAJOR=`echo $JAVA_VERSION_COMPLETE | cut -d. -f 1`
+if [[ $JAVA_VERSION_COMPLETE != $JAVA_MIN && $JAVA_VERSION_MAJOR < $JAVA_MIN_1 ]]
 then
-	echo "To run the current tool, it is necessary to consider Java $JAVA_WANTED at least".
-	echo "Your current Java version is $JAVA_VERSION".
+	echo -e "To run the current tool, it is necessary to have Java >= $JAVA_MIN.\nIt is better Java >= $JAVA_MIN_1."
+	echo "Your current Java version is $JAVA_VERSION_COMPLETE."
 	exit 1
 fi
 
